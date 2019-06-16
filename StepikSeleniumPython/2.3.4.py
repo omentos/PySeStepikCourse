9fc773408b4560fe941028cca44486b966a33b77
Yuriy Prettser <omentos@gmail.com>
2019-06-16 11:21:38 +0600

    Remove unused code from 2.3.4.py

--- StepikSeleniumPython/2.3.4.py
+++ StepikSeleniumPython/2.3.4.py
@@ -20,18 +20,9 @@ x = int(driver.find_element_by_id('input_value').text)
 y = calc(x)
 
 driver.find_element_by_id('answer').send_keys(y)
-# driver.find_element_by_id('robotCheckbox').click()
-# el_radio_robot = driver.find_element_by_id('robotsRule')
-# driver.execute_script('return arguments[0].scrollIntoView(true)', el_radio_robot)
-# el_radio_robot.click()
 
 driver.find_element_by_css_selector('button[type="submit"]').click()
 
-# current_dir = os.path.abspath(os.path.dirname(__file__))  # получаем путь к директории текущего исполняемого файла
-# file_path = os.path.join(current_dir, 'file.txt')
-# print(file_path)
-# print(os.path.abspath(__file__))
-# print(os.path.abspath(os.path.dirname(__file__)))
 
 time.sleep(15)
 driver.quit()
