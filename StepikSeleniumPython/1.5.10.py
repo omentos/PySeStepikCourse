9c404c9b1fe006a7a3d35aa34d90d9dbef3e43f8
Yuriy Prettser <omentos@gmail.com>
2019-06-19 17:53:49 +0600

    Add lesson 1.5 step 10

---
+++ StepikSeleniumPython/1.5.10.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+
+text="test_text"
+link = "http://suninjuly.github.io/registration1.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+browser.find_element_by_css_selector("div.first_block input.first").send_keys(text)
+browser.find_element_by_css_selector("div.first_block input.second").send_keys(text)
+browser.find_element_by_css_selector("div.first_block input.third").send_keys(text)
+
+
+# Отправляем заполненную форму
+button = browser.find_element_by_css_selector("button.btn")
+button.click()
+
+# Проверяем, что смогли зарегистрироваться
+# ждем загрузки страницы
+time.sleep(1)
+
+# находим элемент, содержащий текст
+welcome_text_elt = browser.find_element_by_tag_name("h1")
+# записываем в переменную welcome_text текст из элемента welcome_text_elt
+welcome_text = welcome_text_elt.text
+
+# с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+assert "Поздравляем! Вы успешно зарегистировались!" == welcome_text
+
+
+
